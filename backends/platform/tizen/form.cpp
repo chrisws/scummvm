14dea049755949a0a6c720959e3973f52e201526
Chris Warren-Smith <cwarrensmith@gmail.com>
2013-07-08 21:46:15 +1000

    TIZEN: fix double click change under tizen 2.2

--- backends/platform/tizen/form.cpp
+++ backends/platform/tizen/form.cpp
@@ -334,7 +334,6 @@ int TizenAppForm::getTouchCount() {
 
 void TizenAppForm::OnTouchDoublePressed(const Control &source,
 		const Point &currentPosition, const TouchEventInfo &touchInfo) {
-	logEntered();
 	if (_buttonState != kMoveOnly) {
 		pushEvent(_buttonState == kLeftButton ? Common::EVENT_LBUTTONDOWN : Common::EVENT_RBUTTONDOWN,
 							currentPosition);
@@ -366,7 +365,6 @@ void TizenAppForm::OnTouchMoved(const Control &source,
 
 void TizenAppForm::OnTouchPressed(const Control &source,
 		const Point &currentPosition, const TouchEventInfo &touchInfo) {
-	logEntered();
 	if (getTouchCount() > 1) {
 		_gestureMode = true;
 	} else if (_buttonState != kMoveOnly) {
